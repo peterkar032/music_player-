77811aa4bdf8c95f04a93854111f0470f76bb023
FELIX6402 <trikidisfelinis@gmail.com>
2024-11-17 01:14:06 +0200

    Tab layout was made with 4 tabs. Search, Albums, Favorites, Playlists. Work has been done on Search bar where all the code and ui was transferred there from main. Everything works fine.

--- app/src/main/java/com/example/music/MainActivity.java
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Kωδικας για την λειτουργεια των Tabs
         tabLayout = findViewById(R.id.tabLayout);
         viewPager2 = findViewById(R.id.viewPager);
         viewPagerAdapter = new ViewPagerAdapter(this);
