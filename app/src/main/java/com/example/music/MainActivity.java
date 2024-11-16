181e3303f3d88bed0a2c0a560ed11459140f8274
FELIX6402 <trikidisfelinis@gmail.com>
2024-11-16 22:35:33 +0200

    Tab layout was made with 4 tabs. Search, Albums, Favorites, Playlists. Work has been done on Search bar where all the code and ui was transferred there from main. Everything works fine.

--- app/src/main/java/com/example/music/MainActivity.java
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 
-
 public class MainActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
@@ -47,5 +46,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-}
+    }
 }
